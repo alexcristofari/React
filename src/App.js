4552823d52fb2bb6e0a9b313415e311115a776f0
alex <alexcristofari2@gmail.com>
2025-01-10 00:37:37 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu primeiro projeto React</h1>
+        <p> Meu primeiro App</p>
+    </div>
+  );
+}
+
+export default App;
